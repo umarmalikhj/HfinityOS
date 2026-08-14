2398d371aaf4b3bbc9bde2c270ac3951007996ae
umarmalikhj <umarmalik.hj@gmail.com>
2026-08-14 12:19:39 +0530

    Added Code

--- components/menubar/menubar.js
+++ components/menubar/menubar.js
@@ -5,34 +5,35 @@ function CurrentTime() {
     
     const day = now.getDay();
     const days = {
-        1 : "Sun",
-        2 : "Mon",
-        3 : "Tue",
-        4 : "Wed",
-        5 : "Thu",
-        6 : "Fri",
-        7 : "Sat",
+        0 : "Sun",
+        1 : "Mon",
+        2 : "Tue",
+        3 : "Wed",
+        4 : "Thu",
+        5 : "Fri",
+        6 : "Sat",
     }
 
     const date = now.getDate();
     const month = now.getMonth();
     const months = {
-        1 : "Jan",
-        2 : "Feb",
-        3 : "Mar",
-        4 : "Apr",
-        5 : "May",
-        6 : "Jun",
-        7 : "Jul",
-        8 : "Aug", 
-        9 : "Sep",
-        10 : "Oct",
-        11 : "Nov",
-        12 : "Dec"
+        0 : "Jan",
+        1 : "Feb",
+        2 : "Mar",
+        3 : "Apr",
+        4 : "May",
+        5 : "Jun",
+        6 : "Jul",
+        7 : "Aug", 
+        8 : "Sep",
+        9 : "Oct",
+        10 : "Nov",
+        11 : "Dec"
     }
+
+    const currentMonth = months[month];
+
     const hours = now.getHours();
     const minutes = now.getMinutes();
-
-    return {}
 
 }
